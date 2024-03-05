11bfe095bb842d6fb387f3bbdee2a3184b109cb0
John Pevien Laguindam <johnpevienwork@gmail.com>
2024-03-05 12:15:46 +0000

    add string manipulation file

---
+++ day-1/string-manipulation.py
@@ -0,0 +1,10 @@
+# Print the title of the program
+print("Day 1 - String Manipulation")  # Print the title of the program
+
+# This line prints a message about string concatenation in Python, using the escape character '\' to include the "+" sign in the string.
+print("String Concatenation is done with the \"+\" sign.")
+
+# Print an example of string concatenation
+print('e.g. print("Hello " + "world")') 
+
+print("New lines can be created with a backslash and n.")  
